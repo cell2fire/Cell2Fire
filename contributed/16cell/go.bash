31591e9f19e259479de4890c205690f6a4fdbe89
ulises <ujaimeyepez@ucdavis.edu>
2019-10-04 11:15:16 -0700

    working 16 cell

---
+++ contributed/16cell/go.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+# run from main dir.... Cell2Fire/src/Cell2Fire ... for now
+mydir=../../contributed/16cell
+python main.py --input-instance-folder $mydir/16cellsC1/ --output-folder $mydir/results --ignitions --sim-years 1 --nsims 1 --finalGrid --weather rows --nweathers 1 --Fire-Period-Length 1.0 --output-messages --ROS-CV 0.0 --seed 1134 --stats --allPlots --IgnitionRad 0 --grids --combine --verbose
