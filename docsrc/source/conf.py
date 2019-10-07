15d9fbc1cfbb5fcb22eee90050ef3d327296920c
ulises <ujaimeyepez@ucdavis.edu>
2019-10-07 12:02:21 -0700

    debugging

--- docsrc/source/conf.py
+++ docsrc/source/conf.py
@@ -56,6 +56,7 @@ import sphinx_rtd_theme
 
 html_theme ='sphinx_rtd_theme'
 
+master_doc = "index"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
