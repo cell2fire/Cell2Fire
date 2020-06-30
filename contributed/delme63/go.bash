6c1a690f7f72855fbd6dc9d5bd0fbb3491bafba4
TC-Zheng <eric.tc.zheng@gmail.com>
2020-06-30 14:28:19 -0700

    updated the old path in go.bash

--- contributed/delme63/go.bash
+++ contributed/delme63/go.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
-# run from main dir.... Cell2Fire/src/Cell2Fire ... for now
-mydir=../../contributed/delme63
+# run from main dir.... Cell2Fire/cell2fire ... for now
+mydir=../contributed/delme63
 python main.py --input-instance-folder $mydir/9cellsC1/ --output-folder $mydir/results --ignitions --sim-years 1 --nsims 1 --finalGrid --weather rows --nweathers 1 --Fire-Period-Length 1.0 --output-messages --ROS-CV 0.0 --seed 1134 --stats --allPlots --IgnitionRad 5 --grids --combine --verbose
