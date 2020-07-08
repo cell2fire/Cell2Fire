9253b8b55f853ebaf64a9eb32aa240d22a66ad24
Zonghan Yue <zhyue@ucdavis.edu>
2020-07-09 05:09:02 +0800

    update

--- tests/test_readspot/test_readspot.py
+++ tests/test_readspot/test_readspot.py
@@ -1,12 +1,12 @@
 import  unittest
 import os.path
 import datetime
-from cell2fire.utils.ReadDataPrometheus import ReadDataPrometheus
+from cell2fire.utils.ReadDataPrometheus import ReadData
 filename = "spot_file_complex.json"
 nonoutput = False
 
 if __name__ == "__main__":
     try:    
-        SpottingParams = ReadDataPrometheus.ReadSpotting(filename, nonoutput)
+        SpottingParams = ReadData.ReadSpotting(filename, nonoutput)
     except:
         print("Bug occur")
