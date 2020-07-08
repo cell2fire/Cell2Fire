97ff40c121e7a5328ef820eaf25ae2c8c7472836
Zonghan Yue <zhyue@ucdavis.edu>
2020-07-09 05:15:06 +0800

    update

--- tests/test_readspot/test_readspot.py
+++ tests/test_readspot/test_readspot.py
@@ -1,6 +1,7 @@
 import  unittest
 import os.path
 import datetime
+from cell2fire.utils.ParseInputs import make_parser
 from cell2fire.utils.ReadDataPrometheus import ReadData
 filename = "spot_file_complex.json"
 nonoutput = False
