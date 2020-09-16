bb91bbc550ee69d2eb32e3fd66f107de183f1d01
yzh9810 <zhyue@ucdavis.edu>
2020-09-15 23:28:49 -0700

    ok

--- cell2fire/main.py
+++ cell2fire/main.py
@@ -18,14 +18,14 @@ from cell2fire.utils.ParseInputs import ParseInputs
 #from Cell2Fire.ParseInputs import ParseInputs
 from cell2fire.Cell2FireC_class import *
 from cell2fire.utils.Stats import *
-
+print("here")
 def main():
     # Parse inputs (args)
     args = ParseInputs()
-
+    print("part1")
     # C++ init and run
     env = Cell2FireC(args)
-    
+    print("part2")
     # Postprocessing: Plots Stats
     if args.stats:
         print("------ Generating Statistics --------")
