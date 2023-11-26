d1ae7ac20ed1df052d9aebec39b55f74f7c9e7c7
David L Woodruff <dlwoodruff@ucdavis.edu>
2023-11-26 13:17:18 -0800

    trying to match mpi-sppy doc setup

--- doc/src/conf.py
+++ doc/src/conf.py
@@ -11,8 +11,8 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import sys
+sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
@@ -63,7 +63,7 @@ html_theme ='sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+#html_static_path = ['_static']
 
 
 # -- Options for HTMLHelp output ------------------------------------------
